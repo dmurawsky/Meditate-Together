eee6695654bddbd3c7baae72bc259c1debfdd512
Daniel Murawsky <d.r.murawsky@gmail.com>
2015-03-08 16:21:24 -0400

    added comments

--- app/app.module.js
+++ app/app.module.js
@@ -12,9 +12,11 @@ var app = angular.module('app', ['firebase', 'ngRoute'])
 	});
 })
 .controller("monadCtrl", ["$scope", "$rootScope", "$firebase", "$routeParams", "$location", "$filter", function($scope, $rootScope, $firebase, $routeParams, $location, $filter){
+	//angularFire sync object
 	var ref = new Firebase("https://soil.firebaseio.com/"+$routeParams.monad);
 	var sync = $firebase(ref).$asObject();
 	sync.$bindTo($scope, "monad");
+	//if this is the first time visiting this link add the title property to create the monad model
 	ref.once("value", function(snap){
 		var title = snap.hasChild('title');
 		if(!title){
@@ -22,6 +24,7 @@ var app = angular.module('app', ['firebase', 'ngRoute'])
 			ref.child("title").set(sanTitle);
 		}
 	});
+	//create a connection by adding a connection and relationship and setting the value to true
 	$scope.makeConnection = function(newConnection, newRelationship){
 		var sanConnection = $filter('sanInput')(newConnection);
 		var sanRelationship = $filter('sanInput')(newRelationship);
@@ -31,9 +34,11 @@ var app = angular.module('app', ['firebase', 'ngRoute'])
 	}
 }])
 .controller("viewCtrl", ["$scope", "$rootScope", "$firebase", "$routeParams", "$location", "$filter", function($scope, $rootScope, $firebase, $routeParams, $location, $filter){
+	//angularFire sync object
 	var monadRef = new Firebase("https://soil.firebaseio.com/"+$routeParams.monad);
 	var monadSync = $firebase(monadRef).$asObject();
 	monadSync.$bindTo($scope, "monad");
+	//if this is the first time visiting this link add the title property to create the monad model
 	monadRef.once("value", function(snap){
 		var title = snap.hasChild('title');
 		if(!title){
@@ -41,6 +46,7 @@ var app = angular.module('app', ['firebase', 'ngRoute'])
 			ref.child("title").set(sanTitle);
 		}
 	});
+	//create a connection by adding a connection and relationship and setting the value to true
 	$scope.makeConnection = function(newConnection, newRelationship){
 		var sanConnection = $filter('sanInput')(newConnection);
 		var sanRelationship = $filter('sanInput')(newRelationship);
@@ -52,6 +58,7 @@ var app = angular.module('app', ['firebase', 'ngRoute'])
 .controller("searchCtrl", ["$scope", "$rootScope", "$firebase", function($scope, $rootScope, $firebase){
 
 }])
+//filter for sanitizing strings for links
 .filter("sanInput", function() {
 	return function(input) {
 		input = input.replace(/[^a-z0-9áéíóúñü \.,_-]/gim,"");
