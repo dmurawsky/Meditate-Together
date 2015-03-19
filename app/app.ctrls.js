20098bf3d5c8d97385201520210b9893985b9e1b
Daniel Murawsky <d.r.murawsky@gmail.com>
2015-03-19 03:51:00 -0400

    fixed formForm ctrl

--- app/app.ctrls.js
+++ app/app.ctrls.js
@@ -1,7 +1,8 @@
 app.controller("formForm", ['Soil', function(Soil){
+	var ctrl = this;
 	this.saveData = function(formTitle){
 		var formId = formTitle.replace(/[^a-z0-9]/gi, '');
 		var newIds = Soil.data(false, false, false, 'form', formTitle, formId);
-		this.formTitle = '';
+		ctrl.formTitle = '';
 	}
 }]);
