355b585a0fd1202e68bea0e33ccf091bd6ca7f79
Daniel Murawsky <d.r.murawsky@gmail.com>
2015-03-19 04:32:10 -0400

    arg

--- app/app.ctrls.js
+++ app/app.ctrls.js
@@ -1,8 +1,8 @@
 app.controller("formForm", ['Soil', function(Soil){
 	var test = this;
 	this.saveData = function(formTitle){
-		var formId = formTitle.replace(/[^a-z0-9]/gi, '');
-		var newIds = Soil.data('a', 'a', 'a', 'form', 'formTitle', 'a');
+		//var formId = formTitle.replace(/[^a-z0-9]/gi, '');
+		Soil.data('a', 'a', 'a', 'form', 'formTitle', 'a');
 		test.formTitle = '';
 	}
 }]);
