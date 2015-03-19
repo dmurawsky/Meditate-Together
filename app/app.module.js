c5b0a95a0bd2506eaff09a77c64c294696425d3d
Daniel Murawsky <d.r.murawsky@gmail.com>
2015-03-19 03:33:33 -0400

    Data function
    Added a data function to the soil service and created a form for
    creating a form…

--- app/app.module.js
+++ app/app.module.js
@@ -25,7 +25,7 @@ var app = angular.module('app', ['firebase', 'ngRoute'])
 }])
 .controller("ViewCtrl", ["$scope", "$routeParams", function($scope, $routeParams){
 	//Gets the form name from the url and loads the proper html and ctrl
-	$scope.templateUrl = 'app/components/'+$routeParams.form+'/view.html';
+	$scope.templateUrl = 'app/components/'+$routeParams.form+'/'+$routeParams.view+'.html';
 }])
 .factory('Soil', ["$firebase", function ($firebase){
 	var ref = new Firebase("https://soil.firebaseio.com/");
@@ -35,16 +35,49 @@ var app = angular.module('app', ['firebase', 'ngRoute'])
 			console.log(params);
 			return {form:params[1], data:params[2], view:params[3]};
 		},
-		data: function(form, data, id){
-			//create a new data entry if new data, else update
-			var dataId = null;
-			if(id){
-				dataId = id;
-				ref.child(form+'/'+id).update({data:data});
-			}else{
-				dataId = ref.child(form).push({data:data});
+		data: function(childForm, childData, childId, parentForm, parentData, parentId, callback){
+			//If dataId and parentId are given, then we're just updating 
+			//So go in and update parent data and child data
+			var cId = childId;
+			var pId = parentId;
+			var errorCb = function(error){if(error){console.log(error);}}
+			if(childForm && childData && childId && parentForm && parentData && parentId){
+				ref.child(parentForm+'/'+parentId).update({data:parentData});
+				ref.child(parentForm+'/'+parentId+'/'+childForm+'/'+childId).update({link:childForm+'/'+childId});
+				ref.child(childForm+'/'+childId).update({data:childData,});
+				ref.child(childForm+'/'+childId+'/'+parentForm+'/'+parentId).update({link:parentForm+'/'+parentId});
+			}else if(childForm && childData && childId && parentForm && parentData && !parentId){
+				pId = ref.child(parentForm).push({data:parentData}, function(error){
+					if(error){console.log(error);}else{
+						ref.child(parentForm+'/'+pId.key()+'/'+childForm+'/'+childId).update({link:childForm+'/'+childId}, errorCb);
+						ref.child(childForm+'/'+childId).update({data:childData}, errorCb);
+						ref.child(childForm+'/'+childId+'/'+parentForm+'/'+pId.key()).update({link:parentForm+'/'+pId.key()}, errorCb);
+					}
+				});
+			}else if(childForm && childData && !childId && parentForm && parentData && !parentId){
+				cId = ref.child(childForm).push({data:childData}, function(cError){
+					if(cError){console.log(cError);}else{
+						pId = ref.child(parentForm).push({data:parentData}, function(pError){
+							if(pError){console.log(pError);}else{
+								ref.child(parentForm+'/'+pId.key()+'/'+childForm+'/'+cId.key()).update({link:childForm+'/'+cId.key()}, errorCb);
+								ref.child(childForm+'/'+cId.key()+'/'+parentForm+'/'+pId.key()).update({link:parentForm+'/'+pId.key()}, errorCb);
+							}
+						});
+					}
+				});
+			}else if(childForm && childData && childId && !parentForm && !parentData && !parentId){
+				ref.child(childForm+'/'+childId).update({data:childData}, errorCb);
+			}else if(!childForm && !childData && !childId && parentForm && parentData && parentId){
+				ref.child(parentForm+'/'+parentId).update({data:parentData}, errorCb);
+			}else if(childForm && childData && !childId && !parentForm && !parentData && !parentId){
+				cId = ref.child(childForm).push({data:childData}, errorCb);
+			}else if(!childForm && !childData && !childId && parentForm && parentData && !parentId){
+				pId = ref.child(parentForm).push({data:parentData}, errorCb);
 			}
-			return dataId;
+			if (typeof callback === "function") {
+    			callback();
+			} else {console.log('Failed to run callback');}
+			return {parent:pId,child:cId};
 		},
 		ref: function(){
 			return ref;
