79626651007d2b13174517bf2cdbca21ee441d18
Daniel Murawsky <d.r.murawsky@gmail.com>
2015-03-19 04:15:22 -0400

    nullified

--- app/app.ctrls.js
+++ app/app.ctrls.js
@@ -2,7 +2,7 @@ app.controller("formForm", ['Soil', function(Soil){
 	var ctrl = this;
 	this.saveData = function(formTitle){
 		var formId = formTitle.replace(/[^a-z0-9]/gi, '');
-		var newIds = Soil.data(false, false, false, 'form', formTitle, formId);
+		var newIds = Soil.data(null, null, null, 'form', formTitle, formId);
 		ctrl.formTitle = '';
 	}
 }]);
