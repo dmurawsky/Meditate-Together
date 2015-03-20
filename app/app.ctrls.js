75d8b023f3046c6c03e7769dce5379f560f38b99
Daniel Murawsky <d.r.murawsky@gmail.com>
2015-03-20 03:49:37 -0400

    date

--- app/app.ctrls.js
+++ app/app.ctrls.js
@@ -2,8 +2,8 @@ app.controller("formForm", ['Soil', function(Soil){
 	var test = this;
 	this.saveData = function(title){
 		var formId = title.replace(/[^a-z0-9]/gi, '').toLowerCase();
-		Soil.data(false, false, false, 'form', title, formId);
+		var date = Date.now();
+		Soil.data('Date', date, false, 'form', title, formId);
 		test.formTitle = '';
-
 	}
 }]);
