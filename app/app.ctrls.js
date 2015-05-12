43ffcba0aaa25cbc438fbfee8286ed16cc470bd0
Daniel Murawsky <d.r.murawsky@gmail.com>
2015-05-12 00:20:04 -0400

    agag

--- app/app.ctrls.js
+++ app/app.ctrls.js
@@ -5,12 +5,12 @@ app.controller("HamsasCtrl", ['HYS', function(HYS){
 .controller("MeditationsCtrl", ['HYS', '$scope', '$firebaseObject', function(HYS, $scope, $firebaseObject){
 	//need to run a loaded function that adds the relevant profile data (name, photo, ulr) to the meditation object.
 	var ref = new Firebase(HYS.url);
-	var obj = = $firebaseObject(ref.child("meditations"));
+	var obj = $firebaseObject(ref.child("meditations"));
 	obj.$loaded().then(function() {
 		console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
 		$scope.meditations = obj;
 		angular.forEach(obj, function(value, key) {
-			
+
 			console.log(key, value);
 		});
 	});
